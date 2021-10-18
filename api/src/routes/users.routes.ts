a51a020e4614e78299b86392b338ae9eb98d5949
Leonardo Burati <leonardoburati@hotmail.com>
2021-10-18 11:15:53 -0300

    feat: create users routes

---
+++ api/src/routes/users.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { CreateUserController } from "../modules/accounts/services/createUser/CreateUserController";
+
+const usersRoutes = Router();
+
+const createUserController = new CreateUserController();
+
+usersRoutes.post("/", createUserController.handle);
+
+export { usersRoutes };
