28947926e1af7293440ea3f411ca1e007cda47aa
Leonardo Burati <leonardoburati@hotmail.com>
2021-10-18 11:09:26 -0300

    first commit

---
+++ api/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+import "./database";
+
+import "./shared/container";
+
+import { router } from "./routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3333, () => {
+  console.log("listen on Port: 3333");
+});
